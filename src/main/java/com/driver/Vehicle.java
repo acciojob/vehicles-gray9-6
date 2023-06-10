c938a9bea58e0f719e8ea309b90e92f3a3d27427
Abhay Kumar <kabhay166@gmail.com>
2023-06-10 12:56:01 +0530

    "First Assignment - second push"

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -8,34 +8,11 @@ public class Vehicle {
 
 
     // Getters
-    public String getName() {
-        return name;
-    }
-
-    public int getCurrentSpeed() {
-        return currentSpeed;
-    }
-
     public int getCurrentDirection() {
         return currentDirection;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    public void setCurrentSpeed(int currentSpeed) {
-        this.currentSpeed = currentSpeed;
-    }
-
-    public void setCurrentDirection(int currentDirection) {
-        this.currentDirection = currentDirection;
-    }
-
-
-    public Vehicle(){
-
-    }
     public Vehicle(String name) {
         this.name = name;
         this.currentSpeed = 0;
