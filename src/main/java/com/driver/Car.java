393b8abd4c6bea909952d195051bcb66c50918cd
Abhay Kumar <kabhay166@gmail.com>
2023-06-10 13:22:44 +0530

    "First Assignment - sixth push"

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -25,7 +25,7 @@ public class Car extends Vehicle {
 
     public void changeGear(int newGear){
         this.currentGear = newGear;
-        System.out.println("changeGear method called - The gear is changed to: " + this.currentGear);
+        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
@@ -61,5 +61,35 @@ public class Car extends Vehicle {
 
     public int getSeats() {
         return seats;
+    }
+
+    // setter
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
+
+    public void setManual(boolean manual) {
+        isManual = manual;
+    }
+
+    public void setCurrentGear(int currentGear) {
+        this.currentGear = currentGear;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
     }
 }
